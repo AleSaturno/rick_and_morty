ee0adb6f3a79716988a0169f49f2fa77ac9faa06
AleSaturno <alesaturno64@gmail.com>
2023-05-02 14:57:28 -0300

    Create Card.jsx

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import style from './Card.module.css';
+
+const Card = (props) => {
+   return (
+      
+      <div className= {style.container}>
+         <img src={props.image} alt='Fotito Rick' />
+         <h2>Name: {props.name}</h2>
+         <h2>Status : {props.status}</h2>
+         <h2>Species : {props.species}</h2>
+         <h2>Gender : {props.gender}</h2>
+         <h2>Origin : {props.origin}</h2>
+         <button onClick = {props.onClose}>Cerrar</button>
+      </div>
+   );
+}
+export default Card
