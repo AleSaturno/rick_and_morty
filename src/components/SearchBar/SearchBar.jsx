75893f096555eb3a2bbbf54d54efafd5c8fa0afe
AleSaturno <alesaturno64@gmail.com>
2023-05-02 21:45:18 -0300

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,8 @@
+import style from './SearchBar.module.css'
+
 const SearchBar = (props) => {
    return (
-      <div>
+      <div className={style.container}>
          <input type='search' />
          <button onClick={props.onSearch}>Agregar</button>
       </div>
