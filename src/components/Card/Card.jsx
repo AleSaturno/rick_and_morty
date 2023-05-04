f84206c7ab1dd37eb5cbda5784bf9929c122ceab
AleSaturno <alesaturno64@gmail.com>
2023-05-03 21:51:12 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
 import style from './Card.module.css';
 
-const Card = (props) => {
+const Card = ( {id, name, status, species, gender, origin ,image, onClose}) => {
    return (
       
       <div className= {style.container}>
-         <img src={props.image} alt='Fotito Rick' />
-         <h2>Name: {props.name}</h2>
-         <h2>Status : {props.status}</h2>
-         <h2>Species : {props.species}</h2>
-         <h2>Gender : {props.gender}</h2>
-         <h2>Origin : {props.origin}</h2>
-         <button onClick = {props.onClose}>X</button>
+         <img src= {image} alt='Fotito Rick' />
+         <h2>Name: {name}</h2>
+         <h2>Status : {status}</h2>
+         <h2>Species : {species}</h2>
+         <h2>Gender : {gender}</h2>
+         <h2>Origin : {origin}</h2>
+         <button onClick = {()=> onClose(id)}>X</button>
       </div>
    );
 }
