cacceb7467071e41f0b60f920489428b1d970eef
AleSaturno <alesaturno64@gmail.com>
2023-05-05 09:29:42 -0300

    Update about.jsx

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,9 +1,9 @@
 const About = () =>{
     return(
-        <>
+        <div>
             <h1>Este es el componente About</h1>
             <p>Estamos aprendiendo Routing</p>
-        </>
+        </div>
     )
 }
 
