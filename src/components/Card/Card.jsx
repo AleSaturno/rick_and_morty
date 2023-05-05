46503ce52e66ad82c250d44670b0835f5596beda
AleSaturno <alesaturno64@gmail.com>
2023-05-05 09:29:47 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,14 @@
 import style from './Card.module.css';
+import { Link } from 'react-router-dom';
 
 const Card = ( {id, name, status, species, gender, origin ,image, onClose}) => {
    return (
       
       <div className= {style.container}>
          <img src= {image} alt='Fotito Rick' />
-         <h2>Name: {name}</h2>
+         <Link to={`/detail/${id}`}>
+            <h2>Name: {name}</h2>
+         </Link>
          <h2>Status : {status}</h2>
          <h2>Species : {species}</h2>
          <h2>Gender : {gender}</h2>
