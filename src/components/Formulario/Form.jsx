90fa3b3c576fac34f26ac8cad8289194943b3070
AleSaturno <alesaturno64@gmail.com>
2023-05-06 16:20:08 -0300

    Update Form.jsx

--- src/components/Formulario/Form.jsx
+++ src/components/Formulario/Form.jsx
@@ -1,7 +1,35 @@
 import style from './For.module.css'
 import Imagen from './Imagen/Logo.jpg'
+import { useState } from 'react'
+import validation from '../Validation/Validation'
 
 const Form = () =>{
+    const [errors, setErrors] = useState({});
+
+
+    const [userData , SetUserData] = useState({
+        email : '',
+        password: ''
+
+    });
+
+    const handleChange = (event) =>{
+        SetUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+        setErrors(validation({
+            ...userData,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+
+   
+
+
+
+
     return(
         <div>
             <form className={style.login}>
@@ -11,10 +39,10 @@ const Form = () =>{
                 <h1>Bienvenido</h1>
 
                 <label htmlFor="email"></label>
-                <input type="text" name="email" placeholder="Usuario" />
+                <input type="text" name="email" placeholder="Usuario" value={userData.email} onChange={handleChange} />
 
                 <label htmlFor="password"></label>
-                <input type="password" name="password" placeholder="Contraseña" />
+                <input type="password" name="password" placeholder="Contraseña" value={userData.password} onChange={handleChange}  />
 
                 <button type="submit" className={style.custom}>Iniciar Sesión</button>
                 <br />
