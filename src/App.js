5a3c56e9540c1b77de1f2f43654b5b68770c5dc1
AleSaturno <alesaturno64@gmail.com>
2023-05-07 11:54:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
    const [access , setAccess] = useState(false)
    const navigate = useNavigate();
-   const Email = 'admin';
+   const Email = 'admin@gmail.com';
    const Password = '123456';
 
    const login = (userData) =>{
