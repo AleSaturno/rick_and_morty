a47d68742dcdac03227f19f57cf736f7d59feb7e
AleSaturno <alesaturno64@gmail.com>
2023-05-07 23:28:36 -0300

    Update about.jsx

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,8 +1,15 @@
+import style from './about.module.css'
+
+
+
 const About = () =>{
     return(
-        <div>
+        <div className={style.bloque}>
             <h1>Este es el componente About</h1>
+            <h2>AUN NO SE QUE PONER, AIUDA!!!</h2>
             <p>Estamos aprendiendo Routing</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit, aperiam magnam. Numquam est corrupti magnam porro ut. Iure blanditiis earum consequuntur, error voluptas placeat ipsa eaque? Laudantium iusto assumenda dignissimos?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam cupiditate maiores, commodi ab, a quo fuga deserunt provident error vitae similique doloribus animi quisquam assumenda! Debitis nemo vel earum laborum.</p>
         </div>
     )
 }
