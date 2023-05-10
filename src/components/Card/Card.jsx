d0474ebb112d78a8e0a9e043817ff4c03cb51e20
AleSaturno <alesaturno64@gmail.com>
2023-05-09 23:47:53 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,32 @@
 import style from './Card.module.css';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { addFav } from '../Redux/actions';
+import { removeFav } from '../Redux/actions';
+import { connect } from 'react-redux';
+import { useState} from 'react';
+
+
 
 const Card = ( {id, name, status, species, gender, origin ,image, onClose}) => {
+
+   const [isFav , setIsfav] = useState(false);
+   const handleFavorite = () =>{
+      if(isFav === true){
+         setIsfav(false);
+         removeFav(id);
+      }
+      else if(isFav === false){
+         setIsfav(true);
+         addFav(id);
+
+      }
+
+
+   }
+
+   
+
    return (
       
       <div className= {style.container}>
@@ -14,7 +39,31 @@ const Card = ( {id, name, status, species, gender, origin ,image, onClose}) => {
          <h2>Gender : {gender}</h2>
          <h2>Origin : {origin}</h2>
          <button onClick = {()=> onClose(id)}>X</button>
+         {
+            isFav ? (
+               <button onClick={handleFavorite}>❤️</button>
+            ) : (
+               <button onClick={handleFavorite}>🤍</button>
+            )
+         }  
       </div>
    );
 }
-export default Card
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+     addFav: (id) => {
+       dispatch(addFav(id));
+     },
+     removeFav: (id) => {
+       dispatch(removeFav(id));
+     },
+   };
+ };
+
+
+
+export default connect(
+   null,// me permite acceder al estado global
+   mapDispatchToProps//me permite despachar acciones
+)(Card);
