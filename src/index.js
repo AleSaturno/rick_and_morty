a655284aa0e22e2d9860eb08db9d6f0c2aea2bff
AleSaturno <alesaturno64@gmail.com>
2023-05-09 23:48:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from './components/Redux/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider Store={store}>
+        <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>  
 );
