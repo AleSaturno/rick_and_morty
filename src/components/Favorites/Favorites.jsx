723d472fa3b0cd26d0676198904660cb0b394144
AleSaturno <alesaturno64@gmail.com>
2023-05-11 10:22:30 -0300

    Update Favorites.jsx

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,14 +1,52 @@
 import { connect } from "react-redux";
 import Card from "../Card/Card";
-
-
-
-
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { filterCards, orderCards } from "../Redux/actions";
 
 const Favorite = ({myFavorites}) => {
 
+    const [aux , setAux] = useState(false);
+
+
+
+
+    const dispatch = useDispatch();
+
+    const handleOrder = (event) =>{
+        dispatch(orderCards(event.target.value));
+        setAux(!aux);
+    };
+
+    const handleFilter = (event) =>{
+        dispatch(filterCards(event.target.value))
+    }
+
+
+
+
+
+
     return (
         <div>
+            <select onChange={handleOrder}>
+                <option  value="A">Ascendente</option>
+                <option value="D">Descendente</option>
+            </select>
+
+            <select onChange={handleFilter}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">Unknown</option>
+            </select>
+
+
+
+
+
+
+
             {myFavorites.map((character) => (
             <Card
             id={character.id}
