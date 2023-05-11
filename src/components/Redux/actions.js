218b136e568ba342bbc660083f8856e047cbead8
AleSaturno <alesaturno64@gmail.com>
2023-05-11 10:22:32 -0300

    Update actions.js

--- src/components/Redux/actions.js
+++ src/components/Redux/actions.js
@@ -16,3 +16,15 @@ export const removeFav = (id) =>{
     }
 };
 
+export const filterCards = (gender) =>{
+    return{
+        type: "FILTER", payload: gender
+    }
+}
+
+export const orderCards = (order) =>{
+    return{
+        type: "ORDER" , payload: order  
+    }  
+}
+
