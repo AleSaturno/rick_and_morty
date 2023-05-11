81a91d6036b680880142b9e8c32760f55685ba02
AleSaturno <alesaturno64@gmail.com>
2023-05-11 10:22:34 -0300

    Update reducer.js

--- src/components/Redux/reducer.js
+++ src/components/Redux/reducer.js
@@ -1,7 +1,8 @@
 
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharactersFav: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -9,13 +10,28 @@ const reducer = (state = initialState, action) => {
         case 'ADD_FAV':
             return {
               ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.allCharactersFav, action.payload],
+                allCharactersFav: [...state.allCharactersFav, action.payload]
             }
         case 'REMOVE_FAV':
             return{
                 ...state,
                 myFavorites: state.myFavorites.filter(fav => fav.id!== Number(action.payload))
             }
+        case "FILTER":
+            const allCharactersFiltered = state.allCharactersFav.filter(char => char.gender ===  action.payload)
+            return{
+                ...state,
+                myFavorites: allCharactersFiltered
+            } 
+        case "ORDER":
+            const allCharactersFavCopy = [...state.allCharactersFav];
+            return{
+                ...state,
+                myFavorites: action.payload === "A"
+                ? allCharactersFavCopy.sort((a, b) => a.id < b.id)
+                : allCharactersFavCopy.sort((a, b) => b.id < a.id)
+            }       
         default:
         return {...state}
     }
