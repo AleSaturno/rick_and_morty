291cb8983c5e8232f485892dc3c5abc3e299d768
AleSaturno <alesaturno64@gmail.com>
2023-05-19 09:01:39 -0300

    Create getCharById.js

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+const getCharById = (res, id) => {
+  axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+  .then((result) => result.data )
+  .then(({name, gender, origin, status, image, species}) => {
+    let character = {
+        id,
+        name,
+        gender,
+        origin,
+        image,
+        status,
+        species
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'}).end(JSON.stringify(character))
+  })
+  .catch((error) => res.writeHead(500, {'Content-Type': 'text/plain'}).end(error.message) )
+};
+
+module.exports = {
+  getCharById,
+};
