5e16eb46a9848abe8f346409595d24b6ec483597
AleSaturno <alesaturno64@gmail.com>
2023-05-20 23:46:26 -0300

    Update login.js

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,7 +1,22 @@
-const login = () =>{
+const users = require('../utils/users')
+
+const login = (req , res) =>{
+    const {email, password} = req.query
+
+    const foudUsers = users.find(user => user.email === email && user.password === password); // Va verificar si email y el password, son correctos.
+
+    if(foudUsers){
+        //Esta función verificar si el acceso es correcto
+        res.status(200).json({access: true}); // Esta 
+
+        //Sino verfica si el acceso es Incorrecto
+    }else{
+        res.status(200).json({access: false});
+    }
+
 
 }
 
 module.exports = {
-    
-}
+    login
+};
