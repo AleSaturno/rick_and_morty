c0a8e3d37466218d7eb15158d1ead782e01a0373
AleSaturno <alesaturno64@gmail.com>
2023-05-21 01:39:44 -0300

    Update index.js

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const router = express.Router();
+
+const {getCharById }= require('../controllers/getCharById');
+const {login} = require('../controllers/login');
+const {postFav, deleteFav} = require('../controllers/handleFavorites');
+
+const router = require('express').Router();
+
+router.get('/character/:id', (req, res) => {
+    getCharById(req , res);
+});
+
+router.get('/login', login);
+
+router.post('/fav', (req, res) => {
+    postFav(req , res);
+});
+
+router.delete('/fav/:id', (req, res) => {
+    deleteFav(req , res);
+});
+
+
+module.exports = router
+
 
 //En este código, se importa la función Router de Express y se crea un nuevo enrutador utilizando express.Router(). Luego, se importan los controladores correspondientes: getCharById, login, postFav, y deleteFav.
 
 //Se definen las rutas utilizando los métodos del enrutador (router.get, router.post, router.delete) y se asigna cada ruta a su respectivo controlador.
-
-
-
-const getCharById = require('../controllers/getCharById');
-
-const login = require('../controllers/login');
-
-const { postFav, deleteFav } = require('../controllers/handleFavorites');
-
-
-router.get('/character/:id', getCharById);
-
-router.get('/login', login);
-
-router.post('/fav', postFav);
-
-router.delete('/fav/:id', deleteFav);
-
-
-module.exports ={
-    router
-};
