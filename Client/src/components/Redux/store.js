ab2126f7cc3b8234f6d60b392bcbd6c815e15203
AleSaturno <alesaturno64@gmail.com>
2023-05-25 22:42:12 -0300

    Update store.js

--- Client/src/components/Redux/store.js
+++ Client/src/components/Redux/store.js
@@ -1,6 +1,11 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducer';
+import thunkMiddleware from 'redux-thunk'
 
 
-export const store = createStore(reducer);
- 
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
+export const store = createStore(
+    reducer,
+    composeEnhacer(applyMiddleware(thunkMiddleware))); // Esta linea nos permite hacer peticiones a un servidor
+
