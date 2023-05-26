abb047cb195a48d1c0c397357d605d681717dcdc
AleSaturno <alesaturno64@gmail.com>
2023-05-25 22:42:14 -0300

    Update getCharById.js

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 const URL = 'https://rickandmortyapi.com/api/character';
 
-const getCharById = (req , res) =>{
- const {id} = req.params;
- 
- axios(`${URL}/${id}`)
- .then(response => response.data)
- .then(({status , name , species , origin , image, gender}) =>{
-    if(name){
+const getCharById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await axios(`${URL}/${id}`);
+    const { status, name, species, origin, image, gender } = response.data;
+
+    if (name) {
       const character = {
         id,
         name,
@@ -16,14 +17,18 @@ const getCharById = (req , res) =>{
         image,
         gender,
         status
-      }
+      };
       return res.status(200).json(character);
     }
+    
     return res.status(404).send('Not Found');
-  })
-  .catch(error => res.status(500).send(error.message));
-}
+    
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
 
-module.exports ={
+module.exports = {
   getCharById
-}
+};
+
