63501884cd139fd1b554610eaffbcdb4d928f9a1
AleSaturno <alesaturno64@gmail.com>
2023-06-02 00:52:06 -0300

    Update index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,25 +1,10 @@
-const express = require('express');
-const server = express();
-const router = require('./routes/index')
+const server = require('./app')
+const { conn } = require('./DB_connection');
 const PORT = 3001;
 
-server.use(express.json());
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
 
-server.use('/rickandmorty', router);
+conn.sync({force: true})
 
 server.listen(PORT , () =>{
   console.log(`Server escuchando en el puerto: ${PORT}`)
